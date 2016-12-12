17d62d3b990a9c6ba9c8f30f4d45ca8130efd71b
pete <1343885539@qq.com>
2016-12-12 14:09:02 +0800

    wwbpack基础

--- webpack基础/demo1/src/index.js
+++ webpack基础/demo1/src/index.js
@@ -1,2 +1,10 @@
-var $ = require('jquery');
+import $ from 'jquery';
+
 $('body').html('Hello');
+
+require.ensure([], () => {
+const Button = require('./Components/Button').default;
+const button = new Button('google.com');
+
+button.render('a');
+}, 'button');
--- webpack基础/demo1/webpack.config.js
+++ webpack基础/demo1/webpack.config.js
@@ -3,11 +3,22 @@ module.exports = {
   output: {
     path: 'builds',
     filename: 'bundle.js',
+    publicPath: 'builds/'
   },
   module: {
     loaders:[
       {
         test: /\.js/,
+        loader: 'babel',
+        include: __dirname + '/src',
+      },
+      {
+        test: /\.scss/,
+        loaders: ['style','css','sass'],
+      },
+      {
+        test: /\.html/,
+        loader:'html',
       }
     ]
   }
