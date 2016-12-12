e28c84fffe955ea113c6dac7b73dd9717c3667bc
pete <1343885539@qq.com>
2016-12-12 19:01:26 +0800

    no message

--- webpack基础/demo2/app/Greeter.js
+++ webpack基础/demo2/app/Greeter.js
@@ -1,7 +1,7 @@
-let config = require('./config.json')
+let config = require('./config.json');
 
 module.exports = function() {
   var greet = document.createElement('div');
-  greet.textContent = "Hi there and greetings!";
+  greet.textContent = config.greetText;
   return greet;
 };
