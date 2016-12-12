e5ce58a5dc82e62c83d8f032f506ba427344bd82
pete <1343885539@qq.com>
2016-12-12 17:25:05 +0800

    入门Webpack，看这篇就够了    http://www.jianshu.com/p/42e11515c10f

---
+++ webpack基础/demo2/app/Greeter.js
@@ -0,0 +1,7 @@
+let config = require('./config.json')
+
+module.exports = function() {
+  var greet = document.createElement('div');
+  greet.textContent = "Hi there and greetings!";
+  return greet;
+};
