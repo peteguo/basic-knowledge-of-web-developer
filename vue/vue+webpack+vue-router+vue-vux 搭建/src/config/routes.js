96d0d380944c39b16a73d6d21efcabbdb68ff990
pete <1343885539@qq.com>
2016-12-13 16:56:06 +0800

    vue+webpack+vue-cli+vue-vuex

---
+++ vue/vue+webpack+vue-router+vue-vux 搭建/src/config/routes.js
@@ -0,0 +1,12 @@
+import index from '../page/index'
+import list from '../page/list'
+export default [
+    {
+      path: '/',
+      component: index
+    },
+    {
+      path: '/list',
+      component: list
+    }
+]
