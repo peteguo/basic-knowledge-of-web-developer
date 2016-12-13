4a6ea4cbac941709e8fbccc022e90d270ede3c34
pete <1343885539@qq.com>
2016-12-13 11:02:36 +0800

    入门Webpack，看这篇就够了 www.jianshu.com/p/42e11515c10f

--- webpack基础/demo2/webpack.config.js
+++ webpack基础/demo2/webpack.config.js
@@ -1,8 +1,12 @@
+let webpack = require('webpack');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   devtool:'eval-source-map'
   entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
   output: {
-    path: __dirname + "/public",//打包后的文件存放的地方
+    path: __dirname + "/build",//打包后的文件存放的地方
     filename: "bundle.js"//打包后输出文件的文件名
   }
 
@@ -22,7 +26,16 @@ module.exports = {
         loader: 'style!css?modules'
       }
     ]
-  }
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new ExtractTextPlugin('style.css')
+  ],
 
   devServer: {
     contentBase: "./public",//本地服务器所加载的页面所在的目录
